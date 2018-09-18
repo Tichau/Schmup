28ee016691a20985751f31d0ade4db827b349734
Adrien Allard <adrien.allard.pro@gmail.com>
2018-09-18 08:47:12 +0200

    Rework levels

---
+++ UnityProject/Assets/Scripts/Translate.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Translate.cs" company="AAllard">Copyright AAllard. All rights reserved.</copyright>
+
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed;
+
+	private void Update()
+	{
+	    this.transform.position += this.speed * Time.deltaTime;
+	}
+}
